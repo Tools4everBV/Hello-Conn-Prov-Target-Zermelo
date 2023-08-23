e3e5079567f7f0fc6e03e22bfd64617d9bad130e
Rudolf <r.amersfoort@tools4ever.com>
2023-08-23 16:12:19 +0200

    Updated account object, added null check for department

--- update.ps1
+++ update.ps1
@@ -15,6 +15,7 @@ $auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
 $studentAccount = [PSCustomObject]@{
     userCode  = $p.ExternalId
     firstName = $p.Name.GivenName
+    prefix    = $p.Name.FamilyNamePrefix
     lastName  = $p.Name.FamilyName
     email     = $p.Contact.Business.Email
 }
@@ -23,6 +24,7 @@ $studentAccount = [PSCustomObject]@{
 $userAccount = [PSCustomObject]@{
     code      = $p.ExternalId
     firstName = $p.Name.GivenName
+    prefix    = $p.Name.FamilyNamePrefix
     lastName  = $p.Name.FamilyName
     email     = $p.Contact.Business.Email
 }
@@ -251,6 +253,10 @@ try {
 
     if ($aRef -ne $p.ExternalId){
         throw "aRef [$aRef] does not match with [$($p.ExternalId)]"
+    }
+
+    if ([string]::IsNullOrEmpty($department)) {
+        throw 'Mandatory property [$department] to define the department is empty. Verify your script mapping.'
     }
 
     # Validate the student account
